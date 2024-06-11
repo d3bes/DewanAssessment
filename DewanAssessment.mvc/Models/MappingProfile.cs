502bc196c5d8f9c0f92640bb699835a7446017d4
d3bes <abdo.mohamed6319@gmail.com>
2024-06-11 06:30:53 +0300

    add autoMapper

---
+++ DewanAssessment.mvc/Models/MappingProfile.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DewanAssessment.mvc.ViewModels;
+
+namespace DewanAssessment.mvc.Models
+{
+    public class MappingProfile:Profile
+    {
+
+        public MappingProfile()
+        {
+         CreateMap<Item,ItemVM>();   
+        }
+        
+    }
+}
