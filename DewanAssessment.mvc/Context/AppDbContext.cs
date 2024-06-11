2cdfc9accce40491dceaee161b74a6a2ffca07f4
d3bes <abdo.mohamed6319@gmail.com>
2024-06-11 06:31:27 +0300

    update context

--- DewanAssessment.mvc/Context/AppDbContext.cs
+++ DewanAssessment.mvc/Context/AppDbContext.cs
@@ -1,27 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using DewanAssessment.mvc.Models;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
 
-
-
-namespace DewanAssessment.mvc.Views.Context
+namespace DewanAssessment.mvc.Context
 {
-    public class AppDbContext : DbContext
+
+         public class AppDbContext : DbContext
     {
         
-        public AppDbContext(DbContextOptions options):base (options)
+        public AppDbContext()
+        {
+            
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base (options)
         {
             
         }
 
-         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseMySql("Server=sql.freedb.tech;Port=3306;Database=freedb_dewanAssessment;Uid=freedb_hiiehaejk;Pwd=7U48dq*Mm73*Rft;",
-                new MySqlServerVersion(new Version(8, 0, 21)));
-            }
-        }
-
+// protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//     {
+//         if (!optionsBuilder.IsConfigured)
+//         {
+//             IConfigurationRoot configuration = new ConfigurationBuilder()
+//                .SetBasePath(Directory.GetCurrentDirectory())
+//                .AddJsonFile("appsettings.json")
+//                .Build();
+//             var connectionString = configuration.GetConnectionString("mysql");
+//             optionsBuilder.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+//         }
+//     }
 
         public DbSet<Item> items {get;set;}
             public DbSet<Receipt> Receipts { get; set; }
@@ -31,4 +42,4 @@ namespace DewanAssessment.mvc.Views.Context
 
    
     }
-}
+    }
