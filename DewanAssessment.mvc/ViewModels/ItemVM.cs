4d9a95bd5eb8f8f7a04d442069fca6dcebb75a67
d3bes <abdo.mohamed6319@gmail.com>
2024-06-12 15:03:37 +0300

    update itemVM

--- DewanAssessment.mvc/ViewModels/ItemVM.cs
+++ DewanAssessment.mvc/ViewModels/ItemVM.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,13 @@ namespace DewanAssessment.mvc.ViewModels
     {
           public int Id { get; set; }
     
+     [Required]
+
     public string Name { get; set; }
+     [Required]
     
     public double Price { get; set; }
+     [Required]
     
     public int Quantity { get; set; }
     }
