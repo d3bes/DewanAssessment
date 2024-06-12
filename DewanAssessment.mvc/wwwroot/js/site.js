2641f29d33316165e25b69a6bfcb339cd8640465
d3bes <abdo.mohamed6319@gmail.com>
2024-06-12 15:06:33 +0300

    update site css

--- DewanAssessment.mvc/wwwroot/js/site.js
+++ DewanAssessment.mvc/wwwroot/js/site.js
@@ -1,4 +1,24 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+
+// import DataTable from 'datatables.net-dt';
+// import 'datatables.net-responsive-dt';
+ 
+// let table = new DataTable('#myTable', {
+//     responsive: true
+// });
+
+// (document).ready( function () {
+//     $('#myTable').DataTable();
+// } );
+
+new DataTable('#myTable');
+
+// var DataTable = require( 'datatables.net' );
+// require( 'datatables.net-responsive' );
+ 
+// let table = new DataTable('#myTable', {
+//     responsive: true
+// });
